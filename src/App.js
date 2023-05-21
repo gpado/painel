0286ba722ba3673ce8f4b53cd85df6ca0b241a2d
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2023-05-21 18:11:13 -0300

    keycloak

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginPage from './pages/LoginPage'; 
 import CssBaseline from '@mui/material/CssBaseline';
-
+import './utils/auth.ts'
 
 const App = () => {
   return (
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,8 +2,10 @@ import Keycloak from 'keycloak-js';
 
 // @ts-ignore
 const keycloakConfig = JSON.parse(process.env.REACT_APP_KEYCLOAK_JSON);
-export const keycloak = Keycloak({
+export const keycloak = new Keycloak({
     url: keycloakConfig['auth-server-url'],
     realm: keycloakConfig['realm'],
     clientId: keycloakConfig['resourse']
 })
+
+
