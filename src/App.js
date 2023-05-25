46844f3ac2435855a62c61342c68265a2f07b173
GustavoHerreroNunes <ghnunes2010@gmail.com>
2023-05-25 09:12:06 -0300

    Add: Consult Users Screen - basic structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './utils/auth.ts';
-import { keycloak} from './utils/auth.ts';
+import { keycloak } from './utils/auth.ts';
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 import Login from './pages/Login.jsx';
+import Dashboard from './pages/consultUsers/dashboard.js';
 
 
 function App() {
   return (
     <React.StrictMode>
-      <ReactKeycloakProvider authClient={keycloak}>
+      <Dashboard />
+      {/* <ReactKeycloakProvider authClient={keycloak}>
         <Login />
-      </ReactKeycloakProvider>
+      </ReactKeycloakProvider> */}
     </React.StrictMode>
   );
 }
