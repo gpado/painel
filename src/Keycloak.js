5762a29d99e38e2f39b92756e1587a7e030e73b1
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2023-05-30 19:01:33 -0300

    atualizacao

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
  url: 'https://keycloack-godigital.azurewebsites.net/auth', // URL do servidor Keycloak
  realm: "keycloak-react-auth",
- clientId: "painel-react-auth",
+ clientId: "teste",
 });
 
 export default keycloak;
